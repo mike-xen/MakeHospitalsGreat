b9d43b6d63c4f8119c7f5fca8ad7126570e8b0cb
mike-xen <57603806+mike-xen@users.noreply.github.com>
2020-04-12 14:15:51 +0300

    Add files via upload

---
+++ hackcorona/static/js/mdc_select.js
@@ -0,0 +1,9 @@
+// import {MDCSelect} from '@material/select';
+
+const ftSelectComponent = mdc.select.MDCSelect.attachTo(document.querySelector('.mdc-select'));
+
+// ftSelectComponent.listen('MDCSelect:change', () => {
+//   console.log(ftSelectComponent.value);
+// });
+
+// console.log(mdc);
